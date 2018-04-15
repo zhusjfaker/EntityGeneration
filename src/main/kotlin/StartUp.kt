b68f33f72584750f51aef82de6101eecd1a7fe97
朱仕杰 <zhusj@jiedaibao.com>
2018-04-15 14:38:04 +0800

    zhusj

--- src/main/kotlin/StartUp.kt
+++ src/main/kotlin/StartUp.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
 
     /* 创建实体文件 并写入字段内容 */
     CoreLoader.GenerateFile(
-            projectpath = "E:\\\\git-project\\\\EntityGenrate\\\\src\\\\main\\\\kotlin\\\\output\\\\",
+            projectpath = "/Users/zhushijie/Desktop/gitlab-company/EntityGeneration/src/main/kotlin/output/",
             driver = "com.mysql.cj.jdbc.Driver",
             databasename = "bpm01",
             url = "jdbc:mysql://100.73.17.8:3306/bpm01",
