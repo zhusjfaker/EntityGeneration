8b8fb7a14e142c3d6f3738de35b2108523b5b8a6
zhusjfaker <439453290@qq.com>
2018-05-28 17:47:29 +0800

    zhusj

--- src/main/kotlin/StartUp.kt
+++ src/main/kotlin/StartUp.kt
@@ -1,16 +1,16 @@
-//import entity.core.base.CoreLoader
-//
-//fun main(args: Array<String>) {
-//
-//    /* 创建实体文件 并写入字段内容 */
-//    CoreLoader.GenerateFile(
-//            projectpath = "E:\\git-project\\EntityGenrate\\src\\main\\kotlin\\output\\",
-//            driver = "com.mysql.cj.jdbc.Driver",
-//            databasename = "bpm01",
-//            url = "jdbc:mysql://127.0.0.1:3306/bpm01?serverTimezone=CTT",
-//            username = "root",
-//            password = "123456",
-//            packagename = "bpm6"
-//    )
-//
-//}
+import entity.core.base.CoreLoader
+
+fun main(args: Array<String>) {
+
+    /* 创建实体文件 并写入字段内容 */
+    CoreLoader.GenerateFile(
+            projectpath = "E:\\git-project\\EntityGenrate\\src\\main\\kotlin\\output\\",
+            driver = "com.mysql.cj.jdbc.Driver",
+            databasename = "bpm01",
+            url = "jdbc:mysql://127.0.0.1:3306/bpm01?serverTimezone=CTT",
+            username = "root",
+            password = "123456",
+            packagename = "bpm6"
+    )
+
+}
